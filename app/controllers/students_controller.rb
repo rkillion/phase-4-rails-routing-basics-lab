772a9058bd13cdb91767fccba5fd9b94ca30ca5f
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-20 18:11:03 -0400

    commit tree 73e8224798a8adff306085e89c62c5e096ea8c22

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.all.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade
+        student = Student.all.max_by{|student| student.grade}
+        render json: student
+    end
+end
